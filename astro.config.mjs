57c7610826b103c13cfb170841a37cf91b725b88
Tobias Løfgren <tobloef@gmail.com>
2025-02-25 15:18:22 +0100

    Code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  output: "static",
+  integrations: [
+    expressiveCode({
+      themes: ["everforest-light", "everforest-dark"],
+    }),
+    mdx(),
+  ]
 });
